2eacce5bfad8d64abfbb20d33a2e24a39ac8b3a5
BryceDMonaco <brycedmonaco@gmail.com>
2015-10-17 02:32:25 -0700

    Added project4 (currently incomplete)

---
+++ Project4/array_formatting.c
@@ -0,0 +1,20 @@
+/*
+	Bryce Monaco
+	
+	CS135-1201
+	
+	Array Formatting
+	
+	Compile with: gcc array_formatting.c -o array -std=c99
+*/
+
+#include <stdio.h>
+
+int main (void) {
+	//User defined variables (with placeholder values)
+	int rowAmount = 1, colAmount = 1, newLineAmt = 1, arrayCharSpan = 6;
+	char worldAllign = 'L', betweenChar = '#';
+	
+}
+
+void read_row
