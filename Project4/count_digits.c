c82ad04142994a09907558e88ca6553c7ffbf98b
BryceDMonaco <brycedmonaco@gmail.com>
2015-10-18 02:41:22 -0700

    Completed count_digits

--- Project4/count_digits.c
+++ Project4/count_digits.c
@@ -1,19 +1,40 @@
+/*
+	Bryce Monaco
+	CS135-1201
+	
+	Count Digits (Assignment 1)
+	
+	Compile with: gcc count_digits.c -o count -std=c99
+*/
+
 #include <stdio.h>
 
 	//Initialize and assign values that allow the while loop to run at least once
 	//Woo globals
 	int colAmount = -1, rowAmount = -1;
+	int storageArray[500] = {0}; //This is needed to store any input
+
+//Function Declarations
+int read_row (int rowNum, int colNum);
+int check_input (int checkInt);
 
 int main (void) {
 	
+	int intOccurance[10] = {0}; //Will be used at the end to count the occurance of 0 - 9
+	
+	//Fill storageArray with placeholder values
+	for (int i = 0; i < 500; i++) {
+		storageArray[i] = 9999;
+	
+	}
 	
 	
-	printf("This program counts occurrences of digits 0 through 9 in an NxM array");
+	printf("This program counts occurrences of digits 0 through 9 in an NxM array\n");
 	printf("Enter the size of your array (\"N M\"): ");
 	
 	//Keep waiting for values that fufill the conditions
 	while (!(colAmount >= 0) && !(rowAmount >= 0)) {
-		scanf("%d %d", &colAmount, &rowAmount);
+		scanf("%d %d", &rowAmount, &colAmount);
 		
 		if ((colAmount >= 0) && (rowAmount >= 0)) {
 			printf("Making an array with %d rows and %d columns.\n", rowAmount, colAmount);
@@ -26,11 +47,11 @@ int main (void) {
 	}
 	
 	//Create the array and fill it with zeroes
-	int digitArray[rowAmount][colAmount] = {0};
+	int digitArray[rowAmount][colAmount];
 	
 	//Fill the array with placeholder values
-	for (int i = 0; i < (rowAmount - 1); i++) {
-		for (int ii = 0; ii < (colAmount - 1); ii++) {
+	for (int i = 0; i < (rowAmount); i++) {
+		for (int ii = 0; ii < (colAmount); ii++) {
 			digitArray[i][ii] = 9999; //9999 is a placeholder
 		
 		}
@@ -38,36 +59,80 @@ int main (void) {
 	
 	//Loop to call read_row
 		//
-	for (int i = 0; i < (rowAmount - 1); i++) { //Run once for every row
-		for (int ii = 0; ii < (colAmount -1); ii++) { //Run once for every column in a row
+	for (int i = 0; i < (rowAmount); i++) { //Run once for every row
+		for (int ii = 0; ii < (colAmount); ii++) { //Run once for every column in a row
 			//Assign the array value to the next read value
 			digitArray[i][ii] = read_row(i, ii);
+			
+			intOccurance[digitArray[i][ii]] += 1;
+		}
+	}
+	
+	printf("\n");
+	
+	for (int i = 0; i < rowAmount; i++) {
+		for (int ii = 0; ii < colAmount; ii++) {
+			printf("\t%d", digitArray[i][ii]);
+		
+		}
+		
+		printf("\n");
+		
+	}
+	
+	printf("Total count for each digit:\n");
+	
+	for (int ii = 0; ii < 10; ii++) {
+		if (intOccurance[ii] > 1 || intOccurance[ii] == 0) {
+			printf("Digit %d occurs %d times.\n", ii, intOccurance[ii]);
+		
+		} else {
+			printf("Digit %d occurs %d time.\n", ii, intOccurance[ii]);
 		
 		}
 	}
+	
+	return 0;
+	
 }
 
-//TODO storageArray is wiped at every return, need to find a way to store values
 int read_row (int rowNum, int colNum) {
 	
-	int value = 9999, int isValid = 0;
-	int storageArray[colAmount] = {0};
+	int value = 9999, isValid = 0;
 	
 	if (colNum == 0) { //If it is the first time the row is being read, print the prompt	
 		printf("Enter row %d values (0 - 9, separated by spaces): ", rowNum);
 		
 		while (!isValid) {
-			for (int j = 0; j < (colAmount - 1); j++) { 	
+			for (int j = 0; j < (colAmount); j++) { 	
+				
+				//printf("Scanning[j%d] ", j); Debug output
+				
 				int inputCache = 9999;
 				
 				scanf("%d", &inputCache);
 				
 				if (check_input(inputCache)) {
 					storageArray[j] = inputCache;
-					isValid = 1;
-				
+					
+					if (j == (colAmount-1)) { //If the final number has been parsed successfully, stop the loop
+						isValid = 1;
+					
+					}
+					
 				} else {
-					printf("***%d is not an intenger between 0 and 9\nEnter row %d values  (0 - 9, separated by spaces): ", inputCache, colNum);
+					printf("***%d is not an intenger between 0 and 9***\nEnter row %d values  (0 - 9, separated by spaces): ", inputCache, colNum);
+					
+					isValid = 0;
+					
+					//The remaining values need to be scanned in so that they don't get
+					//picked up in a later scanf
+					int remainingDump = 0;
+					
+					for (int jj = 0; jj < (colAmount - j - 1); jj++) {
+						scanf("%d", &remainingDump);
+						
+					} 
 					
 					break; //Exit the for loop if an invalid value is found
 					
@@ -76,10 +141,7 @@ int read_row (int rowNum, int colNum) {
 		}
 	}
 	
-	
-		
-	
-	return value;
+	return storageArray[colNum];
 
 }
 
@@ -92,5 +154,4 @@ int check_input (int checkInt) {
 		return 0; //Return false
 	
 	}
-
 }
